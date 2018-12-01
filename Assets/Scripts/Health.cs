8da47bce1b131a3b2c549b8fa26316462118c5b2
joubierjc <joubier.j.c@gmail.com>
2018-12-01 16:17:58 +0100

    added onDeath on health

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Health : MonoBehaviour {
 
 	public float maxValue;
 	public float value;
 	public float localDecayMultiplicator;
+	public UnityEvent onDeath;
 
 	private void Update() {
 		value -= Time.deltaTime * localDecayMultiplicator * GameManager.Instance.decayingHealthMultiplicator;
 		if (value < 0) {
-			// todo mort de ce perso
+			onDeath.Invoke();
 		}
 	}
 
