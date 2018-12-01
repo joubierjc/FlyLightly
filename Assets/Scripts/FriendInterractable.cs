1d84734d5d9adc2ce9f5051e652372e10d37e294
joubierjc <joubier.j.c@gmail.com>
2018-12-01 15:55:47 +0100

    interract frined

---
+++ Assets/Scripts/FriendInterractable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceType {
+	None,
+	Ammo,
+	Oil,
+	Food,
+	Coffee
+}
+
+public class FriendInterractable : Interractable {
+
+	public ResourceType ressource;
+
+	public override void Interract() {
+
+	}
+}
--- Assets/Scripts/Interractible.cs
+++ Assets/Scripts/Interractible.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Interractible : MonoBehaviour {
+public abstract class Interractable : MonoBehaviour {
 
 	public abstract void Interract();
 
