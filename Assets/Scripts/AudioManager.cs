327b911ded99db4382210ba65aae028afb6a6d85
Goubi <vtblot@gmail.com>
2018-12-01 16:18:48 +0100

    audioController

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+using UnityEngine.SceneManagement;
+
+public class AudioManager : MonoBehaviour {
+
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+	private void Start()
+	{
+		if (SceneManager.GetActiveScene().name.Equals("Menu"))
+		{
+			Play("menuMusic");
+		}
+	}
+
+	public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Son:" + name + "pas trouvé !");
+            return;
+        }
+
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Son:" + name + "pas trouvé !");
+            return;
+        }
+
+        s.source.Stop();
+    }
+
+    public bool IsPlaying(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Son:" + name + "pas trouvé !");
+            return true;
+        }
+        return s.source.isPlaying;
+    }
+}
