d90b49e3e871e79398cbc0b0045514afca68869f
joubierjc <joubier.j.c@gmail.com>
2018-12-01 15:41:23 +0100

    begining of player controller & ground check bugging

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+
+	private bool grounded;
+	private Vector2 direction;
+	private Rigidbody2D rb2D;
+
+	const float groundedRadius = 0.2f;
+
+	private void Awake() {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() {
+		// GET INPUTS
+		var horizontal = Input.GetAxisRaw("Horizontal");
+		var vertical = grounded && Input.GetButton("Jump") ? 5f : rb2D.velocity.y;
+
+
+		// GROUND CHECK
+		// TODO
+
+		// APPLY MOVEMENT
+		rb2D.velocity = new Vector2(
+			Mathf.Lerp(rb2D.velocity.x, horizontal * moveSpeed, 0.5f),
+			vertical
+		);
+	}
+
+
+
+}
