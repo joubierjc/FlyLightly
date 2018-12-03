63459631ac8e74dc329faef000e44aaa29061c17
joubierjc <joubier.j.c@gmail.com>
2018-12-03 12:20:58 +0100

    hud is now grey on friend death

---
+++ Assets/Scripts/ImageUpdater.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageUpdater : MonoBehaviour {
+	public Image[] imagesToUpdate;
+	public Color toColor;
+
+	public void ApplyChange() {
+		for (int i = 0; i < imagesToUpdate.Length; i++) {
+			imagesToUpdate[i].color = toColor;
+		}
+	}
+}
