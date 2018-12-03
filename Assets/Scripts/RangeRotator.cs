d30c6f60bdcb0b53da10ae8d126817a3b0e41578
joubierjc <joubier.j.c@gmail.com>
2018-12-03 04:07:41 +0100

    tried range rotator

---
+++ Assets/Scripts/RangeRotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class RangeRotator : MonoBehaviour {
+
+	public Vector2 range = new Vector2(-10f, 10f);
+	public float duration = 2f;
+
+	public void Awake() {
+		RotateToMax();
+	}
+
+	private void RotateToMax() {
+		transform.DORotate(new Vector3(0f, range.x, 0f), duration).OnComplete(RotateToMin);
+	}
+
+	private void RotateToMin() {
+		transform.DORotate(new Vector3(0f, range.y, 0f), duration).OnComplete(RotateToMax);
+	}
+}
