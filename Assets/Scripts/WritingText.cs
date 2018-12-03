170c531ed60fcfe0b80fffb26c29e76be97cef66
Goubi <vtblot@gmail.com>
2018-12-03 13:26:53 +0100

    story menu

---
+++ Assets/Scripts/WritingText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class WritingText : MonoBehaviour {
+
+	public Text firstTextToRead;
+	public float writingDuration = 10f;
+
+	public Text button;
+
+	private string endText;
+
+	private void Start()
+	{
+		endText = firstTextToRead.text;
+		firstTextToRead.text = "";
+		firstTextToRead.DOText(endText.ToUpper(), writingDuration).OnComplete(() => {
+			button.text = "PLAY";
+		});
+	}
+}
